178bf3dfdaf77e5c671d7a14f3f3a6de710e9e55
VukolovArtem <vuko.ram55@gmail.com>
2022-04-04 20:20:42 +0300

    Add EvenListener

--- js/index.js
+++ js/index.js
@@ -1,3 +1,48 @@
-const user_1 = {
-  fullName: "Ivanov Ivan Ivanovuch",
+console.dir(document);
+
+const h1 = document.getElementById("mainHeader");
+const [thirdH2, firstH2, secondH2] = document.getElementsByTagName("h2");
+const queryH1 = document.querySelector("#mainHeader");
+const spans = document.querySelectorAll("span");
+
+h1.addEventListener("click", () => {
+  alert("clicked on h1");
+});
+
+const botton1 = document.querySelector(".button");
+
+function enterListener() {
+  alert("НЕ ВЕСИ НАД ДУШОЙ!");
 }
+
+function leaveListener() {
+  alert("Спасибо!");
+}
+
+botton1.addEventListener("mouseenter", enterListener);
+botton1.addEventListener("mouseleave", leaveListener);
+
+botton1.removeEventListener("mouseleave", leaveListener);
+
+
+const btn = document.getElementById('btn');
+let counter = 0;
+
+const counterSpan = document.querySelector('#counterSpan');
+
+const clickHandler=()=>{
+  
+  counterSpan.innerText = ++counter;
+};
+
+const clickNull=()=>{
+  counter = 0;
+  counterSpan.innerText = counter;
+};
+
+
+btn.addEventListener('click', clickHandler);
+
+
+const btn2 = document.getElementById('btn2');
+btn2.addEventListener('click', clickNull);
