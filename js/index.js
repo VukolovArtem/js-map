931b40714eb4b84e6569a3486a7942d53aa073bc
VukolovArtem <vuko.ram55@gmail.com>
2022-04-04 20:45:42 +0300

    Add createElement

--- js/index.js
+++ js/index.js
@@ -1,48 +1,61 @@
 console.dir(document);
 
-const h1 = document.getElementById("mainHeader");
-const [thirdH2, firstH2, secondH2] = document.getElementsByTagName("h2");
-const queryH1 = document.querySelector("#mainHeader");
-const spans = document.querySelectorAll("span");
+// const h1 = document.getElementById("mainHeader");
+// const [thirdH2, firstH2, secondH2] = document.getElementsByTagName("h2");
+// const queryH1 = document.querySelector("#mainHeader");
+// const spans = document.querySelectorAll("span");
 
-h1.addEventListener("click", () => {
-  alert("clicked on h1");
-});
+// h1.addEventListener("click", () => {
+//   alert("clicked on h1");
+// });
 
-const botton1 = document.querySelector(".button");
+// const botton1 = document.querySelector(".button");
 
-function enterListener() {
-  alert("НЕ ВЕСИ НАД ДУШОЙ!");
-}
+// function enterListener() {
+//   alert("НЕ ВЕСИ НАД ДУШОЙ!");
+// }
 
-function leaveListener() {
-  alert("Спасибо!");
-}
+// function leaveListener() {
+//   alert("Спасибо!");
+// }
 
-botton1.addEventListener("mouseenter", enterListener);
-botton1.addEventListener("mouseleave", leaveListener);
+// botton1.addEventListener("mouseenter", enterListener);
+// botton1.addEventListener("mouseleave", leaveListener);
 
-botton1.removeEventListener("mouseleave", leaveListener);
+// botton1.removeEventListener("mouseleave", leaveListener);
+
+
+// const btn = document.getElementById('btn');
+// let counter = 0;
+
+// const counterSpan = document.querySelector('#counterSpan');
+
+// const clickHandler=()=>{
+  
+//   counterSpan.innerText = ++counter;
+// };
+
+// const clickNull=()=>{
+//   counter = 0;
+//   counterSpan.innerText = counter;
+// };
+
+
+// btn.addEventListener('click', clickHandler);
+
+
+// const btn2 = document.getElementById('btn2');
+// btn2.addEventListener('click', clickNull);
 
 
 const btn = document.getElementById('btn');
-let counter = 0;
+const rootElem = document.getElementById('root');
 
-const counterSpan = document.querySelector('#counterSpan');
-
-const clickHandler=()=>{
-  
-  counterSpan.innerText = ++counter;
-};
-
-const clickNull=()=>{
-  counter = 0;
-  counterSpan.innerText = counter;
-};
-
+function clickHandler() {
+  const messageText = prompt('Enter message');
+  const textPar = document.createElement('p');
+  textPar.innerText = messageText;
+  rootElem.append(textPar);
+}
 
 btn.addEventListener('click', clickHandler);
-
-
-const btn2 = document.getElementById('btn2');
-btn2.addEventListener('click', clickNull);
