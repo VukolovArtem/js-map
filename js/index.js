8c20be753050eeaca7f4bad7b6c3661b2dc4a3ea
VukolovArtem <vuko.ram55@gmail.com>
2022-04-14 18:59:37 +0300

    Add 14.04

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
   Есть див с конпками
@@ -42,22 +42,56 @@
 //   }
 // });
 
-const form  = document.getElementById('todo-form');
-const todoList = document.getElementById('todo-list');
+// const form  = document.getElementById('todo-form');
+// const todoList = document.getElementById('todo-list');
 
-form.addEventListener('submit', (e) =>{
-  e.preventDefault();
-  const {
-    target:{ elements },
-    target,
-  } = e;
+// form.addEventListener('submit', (e) =>{
+//   e.preventDefault();
+//   const {
+//     target:{ elements },
+//     target,
+//   } = e;
 
-  const taskText = elements.task.value;
+//   const taskText = elements.task.value;
 
-  todoState.push(taskText);
-  
-  const li = document.createElement('li');
-  li.textContent = taskText;
-  todoList.append(li);
-  target.reset();
-});
+//   todoState.push(taskText);
+
+//   const li = document.createElement('li');
+//   li.textContent = taskText;
+//   todoList.append(li);
+//   target.reset();
+// });
+
+
+
+// Вывести от 1..n
+
+// function intervalCounter(){
+//   console.time('interval timer');
+//   let i =1;
+
+//   const id = setInterval(() => {
+//     console.log(i++);
+//     if (i>20){
+//       clearInterval(id);
+//       console.timeEnd('interval timer');
+//     }
+//   }, 100);
+// }
+
+// intervalCounter();
+
+// function timeoutCounter(i =0){  
+//   console.log(i++);
+//   if (i<=20){
+//     setTimeout(()=>{
+//       timeoutCounter(i);
+//     }, 100);
+//   }else{
+//     console.timeEnd('timeout timer');
+//   }
+// }
+
+
+// console.time('timeout timer');
+// timeoutCounter();
